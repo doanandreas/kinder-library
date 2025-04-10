259e3e2e73ecaba348246c027d2441e3bc9b419e
doanandreas <doanan3736@gmail.com>
2025-04-10 12:28:15 +0700

    handle bad request for insert book

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -35,3 +35,7 @@ func (app *Application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
+
+func (app *Application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -64,7 +64,7 @@ func (app *Application) insertBooksHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
