d4b3d0c5cf016cc4601cbaff64a7be11ef0dd8e6
doanandreas <doanan3736@gmail.com>
2025-04-10 14:24:50 +0700

    connect api to db

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,7 @@ RUN go build -o /kinder-library ./cmd/api
 
 EXPOSE 8080
 
-CMD [ "/kinder-library" ]
+COPY wait-for-it.sh /wait-for-it.sh
+RUN chmod +x /wait-for-it.sh
+
+ENTRYPOINT ["/wait-for-it.sh", "postgres:5432", "--", "/kinder-library"]
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
 
 type Config struct {
 	port string
+	dsn  string
 }
 
 type Application struct {
@@ -21,9 +27,18 @@ type Application struct {
 func main() {
 	cfg := Config{
 		port: os.Getenv("API_PORT"),
+		dsn:  os.Getenv("DATABASE_URL"),
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	logger.Printf("database connection established")
 
 	app := &Application{
 		config: cfg,
@@ -31,11 +46,31 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%s", cfg.port),
-		Handler: app.routes(),
+		Addr:         fmt.Sprintf(":%s", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
 	}
 
-	logger.Printf("Starting server on port %s", srv.Addr)
-	err := srv.ListenAndServe()
+	logger.Printf("starting server on port %s", srv.Addr)
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg Config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
